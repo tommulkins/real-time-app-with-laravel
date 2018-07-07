aabe7ac93176d6c2b14c6280efa7f4b8ff6bc4d6
Tom Mulkins <tom.mulkins@gmail.com>
2018-07-07 15:09:20 -0400

    TM: Added Resource and basic APIs.

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,14 +2,22 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
 use App\User;
+use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    
+    //protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+    protected $guarded = []; //Allow all input
+    
     public function user()
     {
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
     
     public function replies()
@@ -20,5 +28,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 Route::apiResource('/question', 'QuestionController');
