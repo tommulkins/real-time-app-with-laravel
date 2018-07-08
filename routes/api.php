ff28c7ab492da27a5ebbb970c54ea30847f5b17f
Tom Mulkins <tom.mulkins@gmail.com>
2018-07-08 16:46:06 -0400

    TM: JWT Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
 Route::apiResource('/question', 'QuestionController');
-
 Route::apiResource('/category', 'CategoryController');
-
 Route:: apiResource('/question/{question}/reply', 'ReplyController');
-
 Route::post('/like/{reply}', 'LikeController@likeIt');
 Route::delete('/like/{reply}', 'LikeController@unLikeIt');
+
+Route::group([
+    
+    'prefix' => 'auth'
+
+], function () {
+    
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+    
+});
